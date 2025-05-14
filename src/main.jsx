9a10db2211b25b1b90c7747d3cd9fa452211805c
athulyarchandra <athulyarchandra24@gmail.com>
2025-05-14 12:59:25 +0530

    MatrimonySiteFrontEnd

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import './bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ContextAPI from './contexts/ContextAPI.jsx'
 import AuthContextAPI from './contexts/AuthContextAPI.jsx'
 
